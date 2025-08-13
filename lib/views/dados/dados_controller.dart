28219d506ecf4e1b688f6249c0f22ed391e39cd7
JoasGomes <jojoca.vitor777@gmail.com>
2025-08-12 23:45:01 -0300

    Atualização dos dados da página de relacionada aos dados atuais.

--- lib/views/dados/dados_controller.dart
+++ lib/views/dados/dados_controller.dart
@@ -25,20 +25,45 @@ class DadosController extends GetxController {
   }
 
   Future<void> fetchWeatherData() async {
-    final data = await apiService.fetchCurrentWeather("Recife,BR");
-
-    if (data != null) {
-      pressao.value = (data['main']['pressure'] ?? 0).toDouble();
-      umidade.value = data['main']['humidity'] ?? 0;
-      vento.value = ((data['wind']['speed'] ?? 0) as num).round();
-
+    final currentData = await apiService.fetchCurrentWeather("Recife,BR");
+    if (currentData != null) {
+      pressao.value = (currentData['main']['pressure'] ?? 0).toDouble();
+      umidade.value = currentData['main']['humidity'] ?? 0;
+      vento.value = ((currentData['wind']['speed'] ?? 0) as num).round();
       indiceUV.value = "N/D";
+    }
 
-      precipitacaoManha.value = 0;
-      precipitacaoTarde.value = 0;
-      precipitacaoNoite.value = 0;
+    final forecastData = await apiService.fetchWeatherForecast("Recife,BR");
+    if (forecastData != null && forecastData['list'] != null) {
+      List<dynamic> forecastList = forecastData['list'];
+
+      double manhaSum = 0, tardeSum = 0, noiteSum = 0;
+      int manhaCount = 0, tardeCount = 0, noiteCount = 0;
+
+      for (var item in forecastList) {
+        int dt = item['dt'];
+        DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(dt * 1000, isUtc: true).toLocal();
+        double pop = (item['pop'] ?? 0).toDouble();
+
+        int hour = dateTime.hour;
+
+        if (hour >= 6 && hour < 12) {
+          manhaSum += pop;
+          manhaCount++;
+        } else if (hour >= 12 && hour < 18) {
+          tardeSum += pop;
+          tardeCount++;
+        } else if (hour >= 18 && hour < 24) {
+          noiteSum += pop;
+          noiteCount++;
+        }
+      }
+
+      precipitacaoManha.value = manhaCount > 0 ? ((manhaSum / manhaCount) * 100).round() : 0;
+      precipitacaoTarde.value = tardeCount > 0 ? ((tardeSum / tardeCount) * 100).round() : 0;
+      precipitacaoNoite.value = noiteCount > 0 ? ((noiteSum / noiteCount) * 100).round() : 0;
     } else {
-      print("[DadosController] Falha ao carregar dados do tempo.");
+      print("[DadosController] Falha ao carregar dados da previsão.");
     }
   }
 }
