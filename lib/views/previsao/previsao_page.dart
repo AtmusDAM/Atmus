6d15f31d1effec2dc4960bd9b8915c15bb32bdd0
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-20 22:59:41 -0300

    Refactor: Demais diretórios no padrão MVVM

--- lib/views/previsao/previsao_page.dart
+++ lib/views/previsao/previsao_page.dart
@@ -1,4 +1,4 @@
-import 'package:atmus/ui/home/home_controller.dart';
+import 'package:atmus/views/home/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'previsao_controller.dart';
